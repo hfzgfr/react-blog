c1c22db89859f8ca107b781e0ead6a3c1dd2d471
hfzgfr <hfzgfr@gmail.com>
2020-12-30 20:05:09 +0800

    added dependencies

--- react-blog/src/App.js
+++ react-blog/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Header from './navigations/Header';
+import BlogHome from './components/BlogHome';
+import BlogPost from './components/BlogPost';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Hello Index</h1>
+      <BlogHome />
     </div>
   );
 }
